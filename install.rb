8016f32d0090561d24b9cc355b106599d1164a0a
nathan <nathan.humbert@qualitysmith.com>
2009-05-18 08:37:20 -0700

    fixing bug with install script

--- install.rb
+++ install.rb
@@ -1,4 +1,5 @@
+require 'fileutils'
 src_config_file = File.join(File.dirname(__FILE__), "roodi.yml")
-dest_config_file = File.join(dest_dir, "roodi.yml") 
+dest_config_file = File.join("config", "roodi.yml") 
 
 FileUtils.cp(src_config_file, dest_config_file)
